9488ba4a76cd090c3400be078caf40688e63c11a
pkrzekotowski <piotr.krzekotowsk79@gmail.com>
2023-06-29 12:47:13 +0200

    added sending data to airtable as new records in a choosen table

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+import json
 import replicate
+import requests
 
 def run_model_on_image(image_path):
     with open(image_path, "rb") as image_file:
@@ -9,14 +11,40 @@ def run_model_on_image(image_path):
         )
     return output
 
-#Directory where your images are stored (relative to the script)
-image_directory = os.path.join(os.path.dirname(__file__), "Images")
+
+def send_to_airtable(data):
+    airtable_api_url = "https://api.airtable.com/v0/appqLmpIBtfifD9Ob/table" #Use your own airtable API URL
+    api_key = os.environ.get('AIRTABLE_API_KEY')  # Store your API key as an environment variable
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    # Format your data as required by Airtable
+    payload = {
+        "fields": {
+            "Notes": json.dumps(data)
+        }
+    }
+    response = requests.post(airtable_api_url, json=payload, headers=headers)
+
+    # Print status code and response content for troubleshooting
+    print("Status Code:", response.status_code)
+    print("Response Content:", response.content)
+
+
+# Directory where your images are stored (relative to the script)
+image_directory = os.path.join(os.path.dirname(__file__), "Images") # Remember to set your own path
 
 # Get a list of all image paths in the directory
 image_paths = [os.path.join(image_directory, image) for image in os.listdir(image_directory) if image.endswith(('.png', '.jpg', '.jpeg'))]
 
+print("Image paths:", image_paths)  # Let's print out the image paths to make sure they are being read
+
 outputs = []
 for image_path in image_paths:
+    print("Processing image:", image_path)  # Print which image is currently being processed
     output = run_model_on_image(image_path)
     outputs.append(output)
-    print(output)
+    print("Model output:", output)  # Print the output from the model
+    print("Sending data to Airtable")
+    send_to_airtable(output)
